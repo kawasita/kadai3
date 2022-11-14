850562c390b668adc8bc8860cd19281a20d1f066
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-14 00:18:54 +0000

    5th commit controllerの作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show'
+  get 'books/new'
+  get 'books/show'
+  get 'books/index'
+  get 'books/create'
+  get 'books/destroy'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
