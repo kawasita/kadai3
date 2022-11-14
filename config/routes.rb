ecd7fd4403fca2242e93fd66d31a0275e40bd7e0
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-14 03:13:48 +0000

    6th commit アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/update'
-  get 'users/show'
-  get 'books/new'
-  get 'books/show'
-  get 'books/index'
-  get 'books/create'
-  get 'books/destroy'
-  get 'homes/top'
-  get 'homes/about'
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  end
   devise_for :users
+  root to: 'homes#top'
+  get "homes/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
