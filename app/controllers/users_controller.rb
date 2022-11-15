d7cafde1a7779f73c4fd3e38554946f891745165
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-15 10:19:05 +0000

    11th commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def index
+    @books = @user.books
+    @user = User.find(params[:id])
+    @users = User.all
   end
 
   def edit
@@ -10,5 +13,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+  
+  def create
+    @book = @user.book
+  end
+  
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
