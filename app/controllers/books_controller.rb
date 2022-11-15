a87497c58a5a7d9d80dc5be41f6621599261c247
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-15 01:50:26 +0000

    10th commit index左途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def show
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to book_path(@book.id)
   end
 
 　def update
